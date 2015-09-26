7b9923f426fa80a8ed8bd904fe6381f1d0e94f36
gauthamcity12 <gauthamcity12@gmail.com>
2015-09-26 14:23:54 -0400

    finally

--- PowerMove/app/src/main/java/io/github/gauthamcity12/powermove/MainActivity.java
+++ PowerMove/app/src/main/java/io/github/gauthamcity12/powermove/MainActivity.java
@@ -1,23 +1,35 @@
 package io.github.gauthamcity12.powermove;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private EditText editTextInput;
+    private Location mLastLocation;
+    private TextView longitude;
+    private TextView latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        longitude = (TextView) findViewById(R.id.longitude);
+        latitude = (TextView) findViewById(R.id.latitude);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
