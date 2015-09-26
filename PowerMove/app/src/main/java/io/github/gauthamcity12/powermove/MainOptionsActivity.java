b47389b6b2c063af36598dc57e5e742551aa56c2
gauthamcity12 <gauthamcity12@gmail.com>
2015-09-26 16:47:47 -0400

    Yelp functioning

--- PowerMove/app/src/main/java/io/github/gauthamcity12/powermove/MainOptionsActivity.java
+++ PowerMove/app/src/main/java/io/github/gauthamcity12/powermove/MainOptionsActivity.java
@@ -1,37 +1,23 @@
 package io.github.gauthamcity12.powermove;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
-
-    private EditText editTextInput;
-    private Location mLastLocation;
-    private Button mainButton;
+public class MainOptionsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mainButton = (Button) findViewById(R.id.mainButton);
-
+        setContentView(R.layout.activity_main_options);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
+        getMenuInflater().inflate(R.menu.menu_main_options, menu);
         return true;
     }
 
@@ -50,8 +36,5 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(View view){
-        Intent intent = new Intent(this, MainOptionsActivity.class);
-        startActivity(intent);
-    }
+    public 
 }
